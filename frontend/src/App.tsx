2817a8ed737883c2b5d21c66ceb393991917a44f
JamesEjembi <JamesEjembi@users.noreply.github.com>
2026-05-27 12:33:59 +0100

    fix: resolve issue #6

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,25 @@ import IPManagement from './pages/IPManagement'
 import RoyaltyTracking from './pages/RoyaltyTracking'
 import Stakeholders from './pages/Stakeholders'
 import Layout from './components/Layout'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/ip-management" element={<IPManagement />} />
-        <Route path="/royalty-tracking" element={<RoyaltyTracking />} />
-        <Route path="/stakeholders" element={<Stakeholders />} />
-      </Routes>
-    </Layout>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/ip-management" element={<IPManagement />} />
+            <Route path="/royalty-tracking" element={<RoyaltyTracking />} />
+            <Route path="/stakeholders" element={<Stakeholders />} />
+          </Routes>
+        </Layout>
+      } />
+    </Routes>
   )
 }
 
